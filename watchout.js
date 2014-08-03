cba77f76c959dbcaea77f56c33a06e69dbf88e94
lowi0008 <lowi0008@gmail.com>
2014-08-02 17:29:48 -0700

    css added

--- watchout.js
+++ watchout.js
@@ -1,8 +1,9 @@
 // start slingin' some d3 here.
 var highScore = 0;
 
+
+
 var game = function(){
-  var enemies = [];
   var score = 0;
 
   //reset board background post-collision
@@ -22,7 +23,7 @@ var game = function(){
   });
 
   //populate screen with enemies at random positions
-  for(var i = 0; i <= 30; i++){
+  for(var i = 0; i <= 10; i++){
     d3.select('svg').append('circle')
       .attr('class', 'enemy')
       .attr('cx', function(){
@@ -31,17 +32,28 @@ var game = function(){
       .attr('cy', function(){
         return Math.random()*430;
       })
-      .attr('r', 5)
+      .attr('r', 15)
       .attr('filter', 'url(#shuri)')
-      // .style('fill', 'url(Users/student/Code/Geoffrey_Abdallah/2014-07-watchout/shuriken.png)')
   }
+
+    //add Kareem enemy
+    d3.select('svg').append('circle')
+      .attr('class', 'kareem')
+      .attr('cx', function(){
+        return Math.random()*680;
+      })
+      .attr('cy', function(){
+        return Math.random()*430;
+      })
+      .attr('r', 75)
+      .attr('filter', 'url(#kareem)')
 
   //add player to board
   d3.select('svg').append('circle')
     .attr('class', 'player')
     .attr('cx', 300)
     .attr('cy', 300)
-    .attr('r', 10)
+    .attr('r', 30)
     .attr('filter', 'url(#bruce)')
     .call(drag);
 
@@ -53,47 +65,70 @@ var game = function(){
     var playerR = parseInt(d3.select('.player').attr('r'));
     var enemies = d3.selectAll('.enemy');
     var collision = false;
-    while(!collision){
+
       enemies.each(function() {
         var xDist = Math.abs(playerX - d3.select(this).attr('cx'));
         var yDist = Math.abs(playerY - d3.select(this).attr('cy'));
-        var distance = Math.sqrt(xDist^2 + yDist^2);
+        var distance = Math.sqrt((xDist * xDist) + (yDist *yDist));
         var boundary = playerR + parseInt(d3.select(this).attr('r'));
         collision = (distance < boundary);
+        if(collision) {
+          d3.select('.player').transition().attr('filter', 'url(#brucecol)').transition().attr('filter', 'url(#bruce)')
+          d3.select('body').style('background', '#991C20').transition().style('background', 'yellow')
+          if(score > highScore) {
+            highScore = score;
+            d3.select('.high').text(highScore);
+          }
+        score = 0;
+       }
       });
-
-      //if collision occurs => check highScore, reset score
-     if(collision){
-      if(score > highScore) {
-        highScore = score;
-        //TODO: do something visual to board/pieces on collision
-      }
-      score = 0;
-     }
-     //return true/false for collision
-     return collision;
-    }
   };
 
 
   //set intervalic random enemy movement
   setInterval(function(){
     d3.selectAll('.enemy')
-      .transition().duration(1500)
+      .transition().duration(1000)
       .attr('cx', function(){
         return Math.random()*680;
       })
       .attr('cy', function(){
         return Math.random()*430;
       })
-      //transition
-      //set new attributes
   }, 2000);
+
+  setInterval(function(){
+    d3.select('.kareem')
+      .transition().duration(500)
+      .attr('cx', function(){
+        return Math.random()*680;
+      })
+      .attr('cy', function(){
+        return Math.random()*430;
+      })
+  }, 1500);
+
+  setInterval(function(){
+    d3.select('.kareem').transition.duration(200)
+  },250);
+
+  //Kareem's enter exit
+  // setInterval(function(){
+  //   d3.selectAll('.kareem').transition().duration(500)
+  //   .attr('x', function(){
+  //     return Math.random * 680;
+  //   })
+  //   .attr('y', function(){
+  //     return Math.random() * 430;
+  //   });
+
+  // }, 1000);
 
   //check for collisions, increment score
   setInterval(function() {
-    collisionCheck();
-    score++;
+    collisionCheck()
+    score++
+    d3.select('.current').text(score);
   },10);
 };
 
